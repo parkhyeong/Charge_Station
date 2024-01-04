8f29e4b1174bed621ee7759710795eec10cba6e7
권 홍 <141026603+Hong-g7@users.noreply.github.com>
2024-01-04 09:28:02 +0900

    Revert "1월3일 mainpage 커밋"

--- src/main/java/com/multi/tayotayo/mainpage/MainpageDAO.java
+++ src/main/java/com/multi/tayotayo/mainpage/MainpageDAO.java
@@ -1,18 +1,5 @@
 package com.multi.tayotayo.mainpage;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository 
 public class MainpageDAO {
 
-    @Autowired
-    SqlSessionTemplate my;
-
-    public List<MainpageVO> select() {
-        return my.selectList("mainpage.select"); 
-    }
 }
