139856f04d798729273318a8f25f42d1019a5571
Gwon Hong <rnjsghd97@naver.com>
2024-01-12 11:20:12 +0900

    1. 실시간 API데이터 서버에 저장 2. 마커 클릭시 요약정보 기능 구현 3. 마커에 인포 윈도우(기업명) 기능 구현 4. 충전기 상태정보에 따라 사용가능 불가능 마커에 색깔로 구분

---
+++ src/main/java/com/multi/tayotayo/mainpage/DataVO.java
@@ -0,0 +1,35 @@
+package com.multi.tayotayo.mainpage;
+
+public class DataVO {
+	String chgerId;
+	String statId;
+	String stat;
+	public String getChgerId() {
+		return chgerId;
+	}
+	public void setChgerId(String chgerId) {
+		this.chgerId = chgerId;
+	}
+	public String getStatId() {
+		return statId;
+	}
+	public void setStatId(String statId) {
+		this.statId = statId;
+	}
+	public String getStat() {
+		return stat;
+	}
+	public void setStat(String stat) {
+		this.stat = stat;
+	}
+	@Override
+	public String toString() {
+		return "DataVO [chgerId=" + chgerId + ", statId=" + statId + ", stat=" + stat + "]";
+	}
+	
+	
+
+	
+}
+
+	
---
+++ src/main/java/com/multi/tayotayo/mainpage/JoinVO.java
@@ -0,0 +1,134 @@
+package com.multi.tayotayo.mainpage;
+
+public class JoinVO {
+	private String es_statId;
+	private String stat;
+	private String es_gungoo;
+	private String es_addr;
+	private String es_statNm;
+	private String es_faciL;
+	private String es_faciS;
+	private String es_dvcL;
+	private String es_dvcS;
+	private String es_bnm;
+	private String es_busiNm;
+	private String es_output;
+	private String es_chgerType;
+	private String es_limit;
+	private double es_lat;
+	private double es_lon;
+	private double distance;
+	public String getEs_statId() {
+		return es_statId;
+	}
+	public void setEs_statId(String es_statId) {
+		this.es_statId = es_statId;
+	}
+	public String getStat() {
+		return stat;
+	}
+	public void setStat(String stat) {
+		this.stat = stat;
+	}
+	public String getEs_gungoo() {
+		return es_gungoo;
+	}
+	public void setEs_gungoo(String es_gungoo) {
+		this.es_gungoo = es_gungoo;
+	}
+	public String getEs_addr() {
+		return es_addr;
+	}
+	public void setEs_addr(String es_addr) {
+		this.es_addr = es_addr;
+	}
+	public String getEs_statNm() {
+		return es_statNm;
+	}
+	public void setEs_statNm(String es_statNm) {
+		this.es_statNm = es_statNm;
+	}
+	public String getEs_faciL() {
+		return es_faciL;
+	}
+	public void setEs_faciL(String es_faciL) {
+		this.es_faciL = es_faciL;
+	}
+	public String getEs_faciS() {
+		return es_faciS;
+	}
+	public void setEs_faciS(String es_faciS) {
+		this.es_faciS = es_faciS;
+	}
+	public String getEs_dvcL() {
+		return es_dvcL;
+	}
+	public void setEs_dvcL(String es_dvcL) {
+		this.es_dvcL = es_dvcL;
+	}
+	public String getEs_dvcS() {
+		return es_dvcS;
+	}
+	public void setEs_dvcS(String es_dvcS) {
+		this.es_dvcS = es_dvcS;
+	}
+	public String getEs_bnm() {
+		return es_bnm;
+	}
+	public void setEs_bnm(String es_bnm) {
+		this.es_bnm = es_bnm;
+	}
+	public String getEs_busiNm() {
+		return es_busiNm;
+	}
+	public void setEs_busiNm(String es_busiNm) {
+		this.es_busiNm = es_busiNm;
+	}
+	public String getEs_output() {
+		return es_output;
+	}
+	public void setEs_output(String es_output) {
+		this.es_output = es_output;
+	}
+	public String getEs_chgerType() {
+		return es_chgerType;
+	}
+	public void setEs_chgerType(String es_chgerType) {
+		this.es_chgerType = es_chgerType;
+	}
+	public String getEs_limit() {
+		return es_limit;
+	}
+	public void setEs_limit(String es_limit) {
+		this.es_limit = es_limit;
+	}
+	public double getEs_lat() {
+		return es_lat;
+	}
+	public void setEs_lat(double es_lat) {
+		this.es_lat = es_lat;
+	}
+	public double getEs_lon() {
+		return es_lon;
+	}
+	public void setEs_lon(double es_lon) {
+		this.es_lon = es_lon;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	@Override
+	public String toString() {
+		return "JoinVO [es_statId=" + es_statId + ", stat=" + stat + ", es_gungoo=" + es_gungoo + ", es_addr=" + es_addr
+				+ ", es_statNm=" + es_statNm + ", es_faciL=" + es_faciL + ", es_faciS=" + es_faciS + ", es_dvcL="
+				+ es_dvcL + ", es_dvcS=" + es_dvcS + ", es_bnm=" + es_bnm + ", es_busiNm=" + es_busiNm + ", es_output="
+				+ es_output + ", es_chgerType=" + es_chgerType + ", es_limit=" + es_limit + ", es_lat=" + es_lat
+				+ ", es_lon=" + es_lon + ", distance=" + distance + "]";
+	}
+	
+	
+	
+}
