91d477f707cd967085cf18a078511580494c2c45
Gwon Hong <rnjsghd97@naver.com>
2024-01-17 09:26:10 +0900

    1. 사용자 위치 위도 경도 데이터 불러오기 기능구현 2. 사용자 위치에서 가까운 충전소들 추천 기능 구현(수정중) 권홍,고명보(동시작업)

--- src/main/java/com/multi/tayotayo/mainpage/DataDAO.java
+++ src/main/java/com/multi/tayotayo/mainpage/DataDAO.java
@@ -1,6 +1,8 @@
 package com.multi.tayotayo.mainpage;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +14,24 @@ public class DataDAO {
     @Autowired
     SqlSessionTemplate my;
 
-    	public void insert(DataVO dataVO) {
-    		
-    		try {
-    			  my.insert("data.insert", dataVO);		
-    		} catch (Exception e) {
-    			System.out.println(dataVO.getStat() + "에서 예외발생");
-    		}
-    		
-    	}
-    	
-    	  public List<JoinVO> select() {
-    	        return my.selectList("data.select");
-    	    }
+    public void insert(DataVO dataVO) {
+        try {
+            my.insert("data.insert", dataVO);        
+        } catch (Exception e) {
+            System.out.println(dataVO.getStat() + "에서 예외발생");
+        }
+    }
+    
+    public List<JoinVO> select() {
+        return my.selectList("data.select");
+    }
 
-    	    public List<JoinVO> selectlist(String es_statNm) {
-    	        return my.selectList("data.selectlist", es_statNm);
-    	    }
+    public List<JoinVO> selectlist(String es_statNm) {
+        return my.selectList("data.selectlist", es_statNm);
+    }
+
+	 // 내 주변 추천 목록 검색
+	 	public List<JoinVO> myLocationFindRecommand(JoinVO joinVO) {
+	 		return my.selectList("data.myLocationFindRecommand", joinVO);
+	 	}
 }
